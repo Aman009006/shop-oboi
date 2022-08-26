99aaa241e1ee04b83a505e746707eb131637fd08
bakai04 <joldoshovbakai04@gmail.com>
2022-08-26 09:28:54 +0600

    fix bags

--- src/js/toggleFilter.js
+++ src/js/toggleFilter.js
@@ -1,16 +1,17 @@
 const toggleFilter = () => {
-    const closeBtn = document.querySelector('.filter__close');
-    const openBtn = document.querySelector(".open-filter");
+  const closeBtn = document.querySelector(".filter__close");
+  const openBtn = document.querySelector(".open-filter");
 
-    const filter = document.querySelector('.filter');
+  const filter = document.querySelector(".filter");
 
-    closeBtn?.addEventListener('click', () => {
-        filter.classList.toggle("filter--active")
-    });
-    openBtn?.addEventListener('click', () => {
-        filter.classList.toggle("filter--active")
-    });
+  closeBtn?.addEventListener("click", () => {
+    filter.classList.toggle("filter--active");
+    document.body.classList.toggle("remove-scroll");
+  });
+  openBtn?.addEventListener("click", () => {
+    document.body.classList.toggle("remove-scroll");
+    filter.classList.toggle("filter--active");
+  });
+};
 
-}
-
-export default toggleFilter
+export default toggleFilter;
